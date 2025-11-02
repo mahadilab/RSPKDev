1bbf02e5b96486806ede85c8864dc4a81cd90d87
itrspelitakasih <itrspelitakasih@gmail.com>
2025-11-02 23:52:24 +0800

    Master 05b57aa121b978276251e13a499dbefd524cd547

---
+++ src/fungsi/WarnaRSPK.java
@@ -0,0 +1,52 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package fungsi;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author Owner
+ */
+public class WarnaRSPK extends DefaultTableCellRenderer {
+    ////// tambahan untuk men set kolom mana yang mau di ubah warna bersadarkan status
+    public int kolom = 15;
+    public int statrawat = 10;
+//
+    @Override
+    public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus, int row, int column){
+        Component component = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+        if (row % 2 == 1){
+            component.setBackground(new Color(229,248,255));
+//            component.setBackground(new Color(229,246,244));
+        }else{
+            component.setBackground(new Color(255,255,255));
+        }
+        if (table.getValueAt(row, kolom).toString().equals("Sudah Bayar")){
+            component.setBackground(new Color(51, 185, 255));
+        }
+        if (table.getValueAt(row, statrawat).toString().equals("Batal")){
+            component.setBackground(new Color(211,84,0));
+        }
+//        if (isSelected) {
+//            component.setBackground(new Color(148, 198, 238)); // Warna biru untuk baris yang dipilih
+//            component.setForeground(Color.WHITE);           // Warna teks putih agar kontras
+//        }
+// Membuat teks tebal saat baris dipilih
+        if (isSelected) {
+            component.setFont(component.getFont().deriveFont(Font.BOLD));
+        } else {
+            component.setFont(component.getFont().deriveFont(Font.PLAIN));
+        }
+        return component;
+        
+    }
+}
+     
---
+++ src/widget/Table.java
@@ -0,0 +1,53 @@
+package widget;
+
+import java.awt.Color;
+import javax.swing.JTable;
+import java.awt.Component;
+import java.awt.Font;
+import javax.swing.table.DefaultTableCellRenderer;
+
+/**
+ *
+ * @author usu
+ */
+public class Table extends JTable {
+
+    /*
+     * Serial version UID
+     */
+    private static final long serialVersionUID = 1L;
+
+    public Table() {
+        super();
+        //setBackground(new Color(255,235,255));
+        //setGridColor(new Color(245,170,245));
+        //setForeground(new Color(90,90,90));
+        //setBackground(new Color(255,255,255));
+        setGridColor(new Color(255,255,255));
+        //setForeground(new Color(50,50,50));//warna font
+        setFont(new java.awt.Font("Tahoma", 0, 11)); //font isi
+        setRowHeight(24);//tinggi tabel
+        setSelectionBackground(new Color(255,255,255));
+        setSelectionForeground(new Color(0,0,0));//warna saat dipilih
+        //getTableHeader().setFont(new java.awt.Font("Tahoma", 1, 11)); //font judul
+        getTableHeader().setForeground(new Color(255, 255, 255));//warna judul
+        getTableHeader().setBackground(new Color(50,50,50));//bg judul
+        //getTableHeader().setBorder(javax.swing.BorderFactory.createLineBorder(new Color(255,250,250)));
+        getTableHeader().setFont(new java.awt.Font("Tahoma", 1, 11)); //font judul
+        
+        // Tambahkan renderer untuk bold baris yang dipilih
+        setDefaultRenderer(Object.class, new DefaultTableCellRenderer() {
+            @Override
+            public Component getTableCellRendererComponent(JTable table, Object value,
+                    boolean isSelected, boolean hasFocus, int row, int column) {
+                Component c = super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+                if (isSelected) {
+                    c.setFont(c.getFont().deriveFont(Font.BOLD));
+                } else {
+                    c.setFont(c.getFont().deriveFont(Font.PLAIN));
+                }
+                return c;
+            }
+        });
+    }
+}
